9271a58fde09fd1e1fd9f68518092ea84c6f475c
poojabujju <31477611+poojabujju@users.noreply.github.com>
2017-09-03 10:05:33 +0530

    Create bitwise.c

---
+++ bitwise.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+ 
+void swap(int*, int *);
+ 
+void main()
+{
+    int n1, n2;
+    printf("\nEnter two numbers:");
+    scanf("%d %d", &n1, &n2);
+    printf("\nThe numbers before swapping are Number1= %d Number2 %d", n1, n2);
+    swap(&n1, &n2);       
+    printf("\nThe numbers after swapping are Number1= %d Number2 =%d", n1, n2);
+}
+ 
+
+void swap(int *x, int *y)
+{
+    *x = *x ^ *y;
+    *y = *x ^ *y;
+    *x = *x ^ *y;
+}
