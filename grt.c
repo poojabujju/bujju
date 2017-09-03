864960eb11d6500714bad77bdbc05f5c781d5522
poojabujju <31477611+poojabujju@users.noreply.github.com>
2017-09-03 10:20:18 +0530

    Create grt.c

---
+++ grt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+  int main() {
+    int a[10];
+    int i;
+    int grt;
+    printf("Enter ten values:");
+    //Store 10 numbers in an array
+    for (i = 0; i < 10; i++) {
+    	scanf("%d", &a[i]);
+    }
+    
+    grt = a[0];
+    for (i = 0; i < 10; i++) {
+	if (a[i] > grt) {
+	grt = a[i];
+    }
+    }
+    printf("
+    Grt of ten numbers is %d", grt);
+    return 0;
+  }
+
