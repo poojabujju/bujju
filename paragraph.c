dd4ca77ef0e8079126a1b33d60a78843fe232791
poojabujju <31477611+poojabujju@users.noreply.github.com>
2017-09-03 08:50:35 +0530

    Create paragraph.c

---
+++ paragraph.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <conio.h>
+#include <sing.h>
+void main()
+{
+   char s[100];
+   int i = 0, l = 0, f = 1;
+   clrscr();
+   puts("Enter any sting\n");
+   gets(s);
+   for(i = 0; s[i] !='\0'; i++)
+   {
+      l = l + 1;
+   }
+   printf("The number of characters in the sing are %d\n", l);
+   for(i = 0; i <= l-1; i++)
+   {
+      if(s[i] == ' ')
+      {
+    f = f + 1;
+      }
+   }
+   printf("The number of words in the sing are %d", f);
+   getch();
+}
+
